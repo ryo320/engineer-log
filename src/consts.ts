c94fba4af0eb4c3a1228ea9130def01d47f86a89
ryo320 <95510lbs@gmail.com>
2026-02-02 14:12:25 +0900

    Initial commit: 初投稿の追加とプロジェクト名の変更

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Engineer Log';
+export const SITE_DESCRIPTION = '新社会人エンジニアの学びと挑戦の記録';
