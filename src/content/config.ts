7566850f72ebc995fdaa65985fcc896f5a908e3d
ryo320 <95510lbs@gmail.com>
2026-02-03 18:19:39 +0900

    カテゴリ別

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+	type: 'content',
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+		category: z.string(), // これで各記事のカテゴリを認識させます
+	}),
+});
+
+export const collections = { blog };
